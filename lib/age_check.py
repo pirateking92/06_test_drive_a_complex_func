baee21df8f222c7c9e731bd947801a4e514f51af
katrinamichanicou <148572989+katrinamichanicou@users.noreply.github.com>
2024-05-02 22:43:56 +0100

    links added

--- lib/age_check.py
+++ lib/age_check.py
@@ -2,6 +2,17 @@ import re
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
 
+# Info on importing dateutil from here:
+# https://www.influxdata.com/blog/guide-dateutil-module-python/#:~:text=The%20dateutil%20module%20is%20not,normally%20use%20other%20Python%20modules.
+
+# Info on using re.match
+# https://www.guru99.com/python-regular-expressions-complete-tutorial.html
+
+# Convert age to years using dateutil relativedelta
+# https://stackoverflow.com/questions/32083726/how-do-i-convert-days-into-years-and-months-in-python
+
+
+
 def age_check(date):
     if type(date) is not str:
         raise Exception("Incorrect type")
